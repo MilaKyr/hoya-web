e521194edaf1dc38fd51e865d1ad6f93746a5eff
MilaKyr <11293949+MilaKyr@users.noreply.github.com>
2023-11-03 18:16:55 +0100

    Fix error messages

--- webapp/src/errors.rs
+++ webapp/src/errors.rs
@@ -2,9 +2,9 @@ use thiserror::Error;
 
 #[derive(Error, Debug)]
 pub enum Error {
-    #[error("Failed to read with serde: {0}")]
+    #[error("failed to read with serde: {0}")]
     SerdeError(#[from] serde_json::error::Error),
-    #[error("socket address parsing error")]
+    #[error("socket address parsing error: {0}")]
     SocketAddressParsingError(#[from] std::net::AddrParseError),
     #[error("io error: {0}")]
     IoError(#[from] std::io::Error),
