ec020aad7c5e0bedd5b7e2e1dc7206ac13e4053f
MilaKyr <11293949+MilaKyr@users.noreply.github.com>
2024-04-08 18:08:29 +0200

    Update ORM entities

--- webapp/src/db/relational/entities/shop.rs
+++ webapp/src/db/relational/entities/shop.rs
@@ -10,12 +10,37 @@ pub struct Model {
     pub name: String,
     pub url: String,
     pub logo: String,
+    pub last_parsed: Option<DateTime>,
 }
 
 #[derive(Copy, Clone, Debug, EnumIter, DeriveRelation)]
 pub enum Relation {
+    #[sea_orm(has_many = "super::parsingcategory::Entity")]
+    Parsingcategory,
+    #[sea_orm(has_many = "super::parsinglookup::Entity")]
+    Parsinglookup,
+    #[sea_orm(has_many = "super::shopparsingrules::Entity")]
+    Shopparsingrules,
     #[sea_orm(has_many = "super::shopposition::Entity")]
     Shopposition,
+}
+
+impl Related<super::parsingcategory::Entity> for Entity {
+    fn to() -> RelationDef {
+        Relation::Parsingcategory.def()
+    }
+}
+
+impl Related<super::parsinglookup::Entity> for Entity {
+    fn to() -> RelationDef {
+        Relation::Parsinglookup.def()
+    }
+}
+
+impl Related<super::shopparsingrules::Entity> for Entity {
+    fn to() -> RelationDef {
+        Relation::Shopparsingrules.def()
+    }
 }
 
 impl Related<super::shopposition::Entity> for Entity {
